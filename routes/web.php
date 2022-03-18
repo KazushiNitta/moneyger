a354e8b15274a46f79ca44e4bd8c53e063a455d9
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-03-19 01:18:31 +0900

    resourceController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IncomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('income', IncomeController::class)
+->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
